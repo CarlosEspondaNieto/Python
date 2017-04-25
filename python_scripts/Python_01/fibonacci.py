09c53732d61518e16654aaf3f582a4c648ca4e85
Jorge Raze <jorge.alcaraz@beeva.com>
2017-04-24 23:24:47 -0500

    Add last examples

--- python_scripts/Python_01/fibonacci.py
+++ python_scripts/Python_01/fibonacci.py
@@ -5,3 +5,12 @@ limit = int(sys.argv[1])
 while b < limit:
     print(b)
     a, b = b, a+b
+
+
+def fibon(n):
+    a = b = 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+        return result
--- python_scripts/Python_01/number.py
+++ python_scripts/Python_01/number.py
@@ -1,28 +1,45 @@
 class Number:
     def __init__(self, number):
-        self.number = number
+        """The constructor of a Number
+        """
+        try:
+            self.number = float(number)
+        except ValueError as e:
+            print(e)
+            print("That's not a number!")
+            raise ValueError  # Programatically raising an exception
         self.factorial = self.__factorial()
 
     def __str__(self):
+        """__unicode__ in pyton 2.7
+        """
         return str(self.number)
 
     def __factorial(self):
-        num = 1
-        number = self.number
-        while number >= 1:
-            num = num * number
+        """Private method: Returns the factorial of a number
+        """
+        _, number = 1, self.number  # It's common to use an underscore
+        while number >= 1:          # for a value that you're going to throw
+            _ = _ * number
             number = number - 1
-        return num
+        return _
 
     @property
     def as_dict(self):
-        return self.__dict__
+        """Returns an instance of the object as a
+        dictionary
+        """
+        return self.__dict__  # __dict__ magic function
 
     def get_factorial(self):
+        """Gets and print the factorial of the number
+        """
         print("The factorial of {} is {}".format(
-            self.number, self.__factorial()))
+            self.number, self.__factorial()))  # Be careful of indentation
 
     def calculator(self, operation, another_number):
+        """It allows to calculate basic arithmetic operations
+        """
         operations_dict = {
             'times': '*',
             'plus': '+',
@@ -39,6 +56,7 @@ class Number:
             string_to_eval = '%s%s%s' % (self.number,
                                          operations_dict.get(operation),
                                          another_number)
+            print(string_to_eval)
             return eval(string_to_eval)
         else:
             print("It's not possible to perform that operation")
